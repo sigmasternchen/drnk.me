7312e6da5d7740198703bf0a5cd21355e7da9635
overflowerror <mail@overflowerror.com>
2023-12-02 21:30:09 +0100

    feat: Add HTMX dependency

---
+++ bin/fetch-dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wget -O "../html/static/libs/htmx.js" "https://unpkg.com/htmx.org@1.9.9"
--- templates/layout/top.php
+++ templates/layout/top.php
@@ -5,5 +5,6 @@
         <?php echo $data["title"] ?? "drnk.me"; ?>
     </title>
     <link rel="stylesheet" href="/static/css/main.css">
+    <script src="/static/libs/htmx.js"></script>
 </head>
 <body>
