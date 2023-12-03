c250b3934a0c38b1547bc723e52cba34ad4d2b83
overflowerror <mail@overflowerror.com>
2023-12-03 14:34:04 +0100

    feat: Add fontawesome

--- templates/layout/top.php
+++ templates/layout/top.php
@@ -5,6 +5,7 @@
         <?php echo $data["title"] ?? "drnk.me"; ?>
     </title>
     <link rel="stylesheet" href="/static/css/main.css">
+    <link rel="stylesheet" href="/static/css/fontawesome.min.css">
     <script src="/static/libs/htmx.js"></script>
 </head>
 <body>
--- templates/pages/fragments/creation-successful.php
+++ templates/pages/fragments/creation-successful.php
@@ -1,6 +1,6 @@
 <div class="result">
     <a href="<?php echo $data["url"] ?? ""; ?>" target="_blank"><?php echo $data["url"] ?? ""; ?></a>
-    <a href="javascript:copy('.result a')">copy</a>
+    <a href="javascript:copy('.result a')" class="copy"><i class="fa-regular fa-copy"></i></a>
 </div>
 <!-- <?php echo $data["accessKey"] ?? ""; ?> -->
 Click <a hx-get="/?formonly" hx-swap="innerHTML" hx-target=".center-panel" href="/">here</a> to create a new link.
