781b400c1ea2fd2fe10101c27ebfc23872074ec0
overflowerror <mail@overflowerror.com>
2023-12-03 17:04:00 +0100

    feat: Add favicons

--- templates/layout/top.php
+++ templates/layout/top.php
@@ -4,6 +4,11 @@
     <title>
         <?php echo $data["title"] ?? "drnk.me"; ?>
     </title>
+    <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon16.png">
+    <link rel="shortcut icon" href="/static/icons/favicon48.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/favicon180.png">
+    <link rel="manifest" href="/static/icons/site.webmanifest">
     <link rel="stylesheet" href="/static/css/main.css">
     <link rel="stylesheet" href="/static/css/fontawesome.min.css">
     <script src="/static/libs/htmx.js"></script>
