358447514fc4ff1cc75b2619e6d98811430cad75
overflowerror <mail@overflowerror.com>
2023-12-03 14:00:09 +0100

    feat: Formatting for error messages

--- templates/pages/components/url-form.php
+++ templates/pages/components/url-form.php
@@ -1,12 +1,6 @@
-<?php
-    if ($data["error"] ?? false) {
-?>
-        <div class="error">
-            <?php echo $data["error"]; ?>
-        </div>
-<?php
-    }
-?>
+<div class="error <?php echo ($data["error"] ?? "") ? "" : "invisible"; ?>">
+    <?php echo $data["error"] ?? "no error"; ?>
+</div>
 
 <form action="/manage" method="POST" hx-post="/manage" hx-swap="innerHTML" hx-target=".center-panel">
     <input type="text" name="url" value="<?php echo htmlspecialchars($data["url"] ?? ""); ?>" placeholder="https://">
